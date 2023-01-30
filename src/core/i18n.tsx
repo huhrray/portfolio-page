c1954bd7a5d9915ab9bb04ddf6d65b8c69dd3c1f
huhrray <syhuh93@gmail.com>
2023-01-30 21:06:05 +0900

    change default langauge

--- src/core/i18n.tsx
+++ src/core/i18n.tsx
@@ -15,8 +15,8 @@ i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "en", // if you're using a language detector, do not define the lng option
-    fallbackLng: "en",
+    lng: "kr", // if you're using a language detector, do not define the lng option
+    fallbackLng: "kr",
 
     interpolation: {
       escapeValue: false, // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
